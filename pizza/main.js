51ad3e19123a1de4774b42285dfeac5db5a8f43c
mrdevanas <52702295+mrdevanas@users.noreply.github.com>
2024-11-10 21:17:15 +0100

    Add files via upload

---
+++ pizza/main.js
@@ -0,0 +1,34 @@
+function m(){
+
+document.getElementById("pizza").src =
+ 
+"https://www.dominos.jp/ManagedAssets/JP/product/1604/JP_1604_en_hero_2680.png?v326409215";
+
+document.getElementById("text").innerHTML =
+"Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos illo explicabo perferendis eligendi iure vel tenetur voluptates voluptatum, quasi officiis excepturi aperiam minus exercitationem, id omnis accusantium accusamus eos repellendus!"
+document.body.style.backgroundColor = "rgb(178, 178, 178)"
+document.getElementById("l").innerHTML =
+"Pizza margherita"
+}
+function t(){
+
+    document.getElementById("pizza").src =
+     
+    "https://clipart-library.com/images/pT5rB4d7c.png";
+    document.getElementById("text").innerHTML =
+    " Pizza margherita Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos illo explicabo"
+    document.getElementById("l").innerHTML =
+"Pizza Thon"
+document.body.style.backgroundColor = "rgb(87, 103, 0)"
+    }
+    function me(){
+
+        document.getElementById("pizza").src =
+         
+        "https://pluspng.com/img-png/png-hd-pizza-pizza-png-1538.png";
+        document.getElementById("text").innerHTML =
+        "  Dignissimos illo explicabo"
+        document.getElementById("l").innerHTML =
+"Pizza meats"
+document.body.style.backgroundColor = "rgb(160, 51, 0)"
+        }
